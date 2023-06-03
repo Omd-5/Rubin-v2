56de549851140d4bfa7c96f56cb63986bb8b4d40
FOXs <43019044+Omd-5@users.noreply.github.com>
2023-06-03 18:04:11 +0300

    dd

--- src/Myconfig.h
+++ src/Myconfig.h
@@ -1,20 +1,25 @@
-//Big Doors 
+//Big Doors Config
 //-- Left door
-#define minBL 0
-#define maxBL 100
-#define LbigS1pin 12
-#define LbigS2pin 11
+#define minBL 100
+#define maxBL 29
+#define LbigS1pin 4
+#define LbigS2pin 5
+
 //--Right door
 #define minBR 0
 #define maxBR 100
-#define RbigS1pin 12
-#define RbigS2pin 11
+#define RbigS1pin 8
+#define RbigS2pin 9
+//##################################
 
-//
-int minSL= 10;
-int maxSL= 170;
-#define RsmallPin 12
 
-int minSR= 10;
-int maxSR= 170;
-#define LsmallPin 11
+// //Small Doors Config
+
+#define minSL = 10; //--Left door
+#define maxSL = 170;//------------
+
+#define RsmallPin 2
+#define LsmallPin 4
+
+#define mineSR= 0; //--Right door
+#define maxeSR= 0; //-------------
--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,187 @@
 #include <Arduino.h>
-#include <ESP32Servo.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Myconfig.h>
-
-Servo lsb1, lsb2;
-Servo rsb1, rsb2;
-
-Servo RbigS1, RbigS2;
-
-//Small
-Servo Rsmall;
-Servo Lsmall;
-
-
-TwoWire Wi = TwoWire(0);
-
-#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(128, 64, &Wi, -1);
+// #include <ESP32Servo.h>
+// #include <Servo.h>
+// #include <Adafruit_GFX.h>
+// #include <Adafruit_SSD1306.h>
+// #include <FreeRTOS.h>
+// #include <Wire.h>
+// #include <RCWL_1X05.h>
 
 
 
-void LDoor(int dgree){
-    if (dgree < 181){
-      lsb1.write(dgree);
-      lsb2.write(180 - dgree);
-    }
-}
 
-void RDoor(int dgree){
-    if (dgree < 181){
-      rsb1.write(dgree);
-      rsb2.write(180 - dgree);
-    }
-}
-
-void Lbig(bool state){
-  if (state){
-    LDoor(maxBL);
-  }else{
-    LDoor(minBL);
-  }
-
-}
+// // #Project Files
+// // #include <hundle.h>
+// #include <Myconfig.h>
 
 
-void Rbig(bool state){
-  if (state){
-    RDoor(maxBR);
-  }else{
-    RDoor(minBR);
-  }
+// // RCWL_1X05 sensor;
 
-}
+// //Big Doors Servos
+// Servo lsb1, lsb2;
+// Servo rsb1, rsb2;
 
 
+// //Small Doors Servos
+// Servo Rsmall;
+// Servo Lsmall;
+
+
+// // TwoWire Wi = TwoWire(0);
+
+// // #define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+// // Adafruit_SSD1306 display(128, 64, &Wi, -1);
+
+
+// // #include <ESP32Servo.h>
+// #include <Arduino.h>
+// #include <Myconfig.h>
+
+
+
+// //Big Doors Functions
+// void LDoor(int dgree){
+  
+//     if (dgree < 181){
+//       lsb1.write(dgree);
+//       lsb2.write(180 - dgree);
+//     }
+//   // lsb1.detach();
+//   // lsb2.detach();
+// }
+
+
+// void RDoor(int dgree){
+//   // int dgree = );
+
+//     if (dgree < 181){
+//       rsb1.write(dgree);
+//       rsb2.write(180 - dgree);
+//     }
+//   // rsb1.detach();
+//   // rsb2.detach();
+// }
+
+
+
+// //##################################
+
+// //Small Doors Functions
+// void Lbig(bool state){
+//   if (state){
+//     LDoor(maxBL);
+//   }else{
+//     LDoor(minBL);
+//   }
+// }
+
+
+// void Rbig(bool state){
+
+//   if (state){
+//     RDoor(maxBR);
+//   }else{
+//     RDoor(minBR);
+//   }
+
+// }
+
+// //##################################
+
+// const int pingPin = 20;
+// int inPin = 21;
+
+
+// long microsecondsToCentimeters(long microseconds)
+// {
+//   return microseconds / 29 / 2;
+// }
+
+
+
+
+// void setup(){
+//   // Wire.begin();
+  
+//   // sensor.setFilter(true); // filter is turned off by default
+//   // sensor.setTimeout(50); // 100 ms is recommended, but lower values might work
+//   // sensor.setMode(RCWL_1X05::continuous); 
+//   // Wi.begin(4, 15)1060;
+//   // display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
+//   // rsb1.attach(RsmallPin);
+//   // lsb1.attach(LsmallPin);
+//   rsb1.attach(RbigS1pin);
+//   rsb2.attach(RbigS2pin);
+//   lsb1.attach(LbigS1pin);
+//   lsb2.attach(LbigS2pin);
+
+//   Serial.begin(9600);
+// }
+
+// void loop(){
+//   long duration, cm;
+//   pinMode(pingPin, OUTPUT);
+//   digitalWrite(pingPin, LOW);
+//   delayMicroseconds(2);
+//   digitalWrite(pingPin, HIGH);
+//   delayMicroseconds(5);
+//   digitalWrite(pingPin, LOW);
+//   pinMode(pingPin, INPUT);
+//   duration = pulseIn(pingPin, HIGH);
+//   cm = microsecondsToCentimeters(duration);
+//   Serial.print(cm);
+//   Serial.print("cm");
+//   // if (sensor.update()) {
+//   //   Serial.print(sensor.read()); Serial.println(" mm");
+  
+//   if(Serial.available()){
+//     Serial.print(cm);
+//     Serial.print("cm");
+//   }
+//   for (int i = 0; i < 180; i += 100)
+//   {
+//     LDoor(i);
+//     delay(10);
+//     RDoor(i + 30);
+//     delay(10);
+//   }
+// }
+
+
+
+
+
+
+// void setup(){
+//   Serial.begin(115200);
+//   pinMode(18, INPUT);
+//   pinMode(33, INPUT);
+//   pinmode(35, INPUT);
+
+//   }
+
+// void loop(){
+//   Serial.println( analogRead(18));
+
+// }
 
 
 void setup(){
-  Wi.begin(4, 15);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
-  lsb1.attach(LbigS1pin);
-  lsb2.attach(LbigS2pin);
-  rsb1.attach(RbigS1pin);
-  rsb2.attach(RbigS2pin);
-  Serial.begin(115200);
+  Serial.begin(9600);
+  pinMode(A3, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A0, INPUT);
 }
 
 void loop(){
-  if(Serial.available()){
-      int state = Serial.parseInt();
-      Serial.println(state);
-      LDoor(state);
-  }
-  
-
+  Serial.print("A0: ");
+  Serial.print(analogRead(A0));
+  Serial.print(" A1: ");
+  Serial.print(analogRead(A1));
+  Serial.print(" A2: ");
+  Serial.print(analogRead(A2));
+  Serial.print(" A3: ");
+  Serial.println(analogRead(A3));
+  delay(500);
 }
